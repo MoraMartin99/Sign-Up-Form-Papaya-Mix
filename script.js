1dd3525205ab488102f4594de221e9dbd0753db6
Oscar Martin Mora <moramartin99@gmail.com>
2023-03-07 15:13:23 -0600

    Agregar variables globales a script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Variables globales */
+/* -------------------------------------------------------------------------------------------------- */
+
+const form = document.querySelector("form");
+const inputArr = Array.from(document.querySelectorAll("#form input"));
+const createButton = document.querySelector("#createButton");
+
+/* -------------------------------------------------------------------------------------------------- */
+
