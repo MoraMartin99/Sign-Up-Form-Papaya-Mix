f5c8882d5b934951ad588ca5da00db20a6808546
Oscar Martin Mora <moramartin99@gmail.com>
2023-03-07 15:21:41 -0600

    Agregar los tests en script.js

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ const createButton = document.querySelector("#createButton");
 
 /* -------------------------------------------------------------------------------------------------- */
 
+/* Tests */
+/* -------------------------------------------------------------------------------------------------- */
+
+const isValidHTMLInput = (input) => {
+    input.setCustomValidity("");
+    return input.checkValidity();
+};
+
+const hasMinimumLength = (input) => {
+    const value = input.value;
+    const lengthReg = /^\S{8,}$/;
+    return lengthReg.test(value);
+};
+
+const hasUpperAndLowerCase = (input) => {
+    const value = input.value;
+    const lowerReg = /[a-z]/;
+    const upperReg = /[A-Z]/;
+    return lowerReg.test(value) && upperReg.test(value);
+};
+
+const hasDigit = (input) => {
+    const value = input.value;
+    const digReg = /\d/;
+    return digReg.test(value);
+};
+
+const hasSpecialChar = (input) => {
+    const value = input.value;
+    const specialReg = /[^a-z0-9\s]/i;
+    return specialReg.test(value);
+};
+
+const areSamePassword = (input) => {
+    const value = input.value;
+    const password = document.querySelector("#contraseña").value;
+    if (value === "") return false;
+    return value === password;
+};
+
+/* -------------------------------------------------------------------------------------------------- */
+
